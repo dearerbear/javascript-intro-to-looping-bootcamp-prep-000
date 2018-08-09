18a712499286c4336478f5e826d037772ee14f93
Dear Bear <dearerbear@gmail.com>
2018-08-09 03:46:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,7 +13,7 @@ function forLoop(array) {
   for (let i=0;  i < 25; i++) {
     array.push(`I am ${i} strange loop${ i === 0 ? '' : 's'}.`)
   }
-  console.log(array)
+  return array
 }
 
 forLoop()
